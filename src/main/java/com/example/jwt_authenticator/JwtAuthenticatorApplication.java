865668de82963f977251af09265513ec84080c3e
YassineIdiri <pro.yassine.idiri@gmail.com>
2026-02-08 15:53:09 +0100

    initialize project

---
+++ src/main/java/com/example/jwt_authenticator/JwtAuthenticatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwt_authenticator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthenticatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthenticatorApplication.class, args);
+	}
+
+}
