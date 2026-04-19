e3a2ea50a73a81e343b294c7f1b7f314f70ae8d1
YassineIdiri <pro.yassine.idiri@gmail.com>
2026-04-19 16:30:26 +0200

    feature: update security

--- src/test/java/com/example/jwt_authenticator/JwtAuthenticatorApplicationTests.java
+++ src/test/java/com/example/jwt_authenticator/JwtAuthenticatorApplicationTests.java
@@ -2,12 +2,13 @@ package com.example.jwt_authenticator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-class JwtAuthenticatorApplicationTests {
+@ActiveProfiles("test")
+class JwtAuthenticatorApplicationTests extends AbstractIntegrationTest {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // Vérifie juste que le contexte Spring démarre sans erreur
+    }
 }
